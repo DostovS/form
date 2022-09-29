cc56221fe0b049903006e67899a2090e9fdd3e2e
DostovS <sohibjondostov04@gmail.com>
2022-09-29 13:03:57 +0500

    success

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,13 +1,3 @@
 import axios from "axios";
-// import {state} from '../store/store'
-// const getToken = ()=>{
-//     return  token;
-// }
-
 axios.defaults.baseURL = "https://auth-app-itransition.netlify.app/";
-// axios.interceptors.request.use(async(config) =>{
-//     console.log(getToken());
-//     // const token =
-// })
-
 export default axios;
